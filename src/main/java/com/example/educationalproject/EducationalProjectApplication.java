91d86d760fb847e6ee7fabc333a3c9b870397dd8
Ruslan Sidorov <rusl.1193@gmail.com>
2025-11-02 11:52:18 +0300

    init commit

---
+++ src/main/java/com/example/educationalproject/EducationalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/educationalproject/EducationalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
